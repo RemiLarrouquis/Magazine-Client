d4d8b81ff0de76bd5fa0080bd7a2a3c7e6e83820
LudoNeckat <ludovicgirard@gmail.com>
2017-07-07 14:23:39 +0200

    change common.js

--- js/commons.js
+++ js/commons.js
@@ -15,6 +15,9 @@ var commons = {
 
     url: function (route) {
         // var base = "magazine.dev";
+
+        //Ligne à changer pour faire marcher le projet
+        //Modifier l'ip par l'ip sur lequel le serveur laravel tourne
         var base = "10.0.10.120/Magazine/public";
 
         return "http://" + base + "/api" + route;
